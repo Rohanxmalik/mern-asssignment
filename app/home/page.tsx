c86a55114430c92c5711e5c836fbba04b3a3da6b
Snehil Shrivastava <snehilshrivastava3@gmail.com>
2024-10-30 14:28:03 +0530

    building home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,13 +1,22 @@
-import React from "react";
+import WeekCalendar from "@/components/WeekCalendar";
+import WeeklyTaskSummary from "@/components/WeeklyTaskSummary";
+import { Search } from "lucide-react";
 
 const Home = () => {
   return (
-    <div className="flex flex-col justify-center items-center w-[390px] h-[844px] bg-white absolute">
-      <input
-        type="text"
-        placeholder="Search for a task"
-        className="w-[339px] p-3 border rounded absolute top-[37px]"
-      />
+    <div className="flex flex-col justify-start items-center w-[390px] h-[844px] bg-white">
+      <div className="flex flex-row justify-center items-center w-[339px] border rounded p-3 mt-[37px]">
+        <input
+          type="text"
+          placeholder="Search for a task"
+          className="w-full focus:border-none focus:outline-none"
+        />
+        <Search className="pl-1 cursor-pointer" />
+      </div>
+      <div className="w-[355px] mt-3">
+        <WeekCalendar />
+        <WeeklyTaskSummary completedTasks={12} pendingTasks={3} />
+      </div>
     </div>
   );
 };
